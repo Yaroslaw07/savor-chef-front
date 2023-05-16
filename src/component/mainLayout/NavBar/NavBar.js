b4d0777a21fc5a578f5c085e4a71b62941a51d95
Yaroslav_Ustinov <y.ustinov2004@gmail.com>
2023-05-16 16:45:20 +0200

    -- added logout to navbar

--- src/component/mainLayout/NavBar/NavBar.js
+++ src/component/mainLayout/NavBar/NavBar.js
@@ -1,6 +1,8 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import ActivePath from "./ActivePath";
 import InactivePath from "./InactivePath";
+import { useContext } from "react";
+import AuthContext from "../../../contexts/AuthContext";
 
 const pages = [{title:"Home",path:"/"},
                {title:"Shelf",path:"/shelf"}]
@@ -9,9 +11,9 @@ const pages = [{title:"Home",path:"/"},
 function NavBar() {
 
   const location = useLocation();
+  const authCtx = useContext(AuthContext)
 
   console.log(location)
-
 
   return (
     <nav className="bg-white dark:bg-gray-900 w-full z-20 top-0 left-0 border-b-4 border-gray-200 dark:border-gray-600">
@@ -23,25 +25,41 @@ function NavBar() {
           {"Savor-Chef"}
         </Link>
 
-        <div className="flex md:order-2">
-          <button
-            type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Sign In
-          </button>
-        </div>
+        {authCtx.user === null ? (
+          <div className="flex md:order-2 gap-2">
+            <Link
+              to="/signin"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Sign In
+            </Link>
+            <Link
+              to="signup"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Sign Up
+            </Link>
+          </div>
+        ) : (
+          <div className="flex md:order-2 gap-2">
+            <p className="font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0">
+              {authCtx.user.Email}
+            </p>
+            <button
+              onClick={authCtx.logout}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Logout
+            </button>
+          </div>
+        )}
 
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-           
-            {pages.map((page,index) => {
+            {pages.map((page, index) => {
               return location.pathname === page.path ? (
                 <li key={index}>
-                  <ActivePath
-                    title = {page.title}
-                    to = {page.path}>
-                  </ActivePath>
+                  <ActivePath title={page.title} to={page.path}></ActivePath>
                 </li>
               ) : (
                 <li key={index}>
@@ -51,8 +69,7 @@ function NavBar() {
                   ></InactivePath>
                 </li>
               );
-            }
-            )}
+            })}
           </ul>
         </div>
       </div>
