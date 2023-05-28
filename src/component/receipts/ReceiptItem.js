21168fa90fca384ba19f80a8748dbc1fd004173e
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2023-05-28 18:46:21 +0200

    add normal gripCss to Receipt List

--- src/component/receipts/ReceiptItem.js
+++ src/component/receipts/ReceiptItem.js
@@ -3,8 +3,8 @@ import Card from "../ui/Card"
 
 function ReceiptItem(props){
     return (
-      <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        {props.name}
+      <div className="w-60 h-32 p-6  bg-white border border-gray-200 rounded-lg shadow dark: dark:bg-gray-800 dark:border-gray-700">
+        {props.receipt.name}
       </div>
     );
 } 
--- src/component/receipts/ReceiptList.js
+++ src/component/receipts/ReceiptList.js
@@ -2,12 +2,12 @@ import ReceiptItem from "./ReceiptItem"
 
 function ReceiptList(props) {
     return (
-    <ul>
-        {props.receipts.map(
-            receipt => <ReceiptItem key = {receipt.id} name={receipt.name}></ReceiptItem>
-        )}
-    </ul>
-    )
+      <div className="grid grid-cols-1 pt-4 px-2 h-full sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center gap-5">
+        {props.receipts.map((receipt) => (
+          <ReceiptItem key={receipt.id} receipt={receipt}></ReceiptItem>
+        ))}
+      </div>
+    );
 }
 
 export default ReceiptList
